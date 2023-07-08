87e8184c859b8b5e36df95edea5ab8bacdbea9cd
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-08 14:56:49 +0500

    utf16 parser

---
+++ FormatParser.Text/TextChecker.cs
@@ -0,0 +1,19 @@
+namespace FormatParser.Text;
+
+public class TextChecker
+{
+    private static HashSet<uint> NonTextC0Controls =
+        Enumerable.Range(0, 32)
+            .Select(x => (uint) x)
+            .Except(new uint[]
+            {
+               10, // CR
+               13, // LF
+               09, // \t
+               27, // ESC
+               0x0C,
+            })
+            .ToHashSet();
+    
+    public bool IsValidCodepoint(uint codepoint) => !NonTextC0Controls.Contains(codepoint);
+}
---
+++ FormatParser.Text/Utf16Parser.cs
@@ -0,0 +1,57 @@
+﻿namespace FormatParser.Text;
+
+public class Utf16Parser
+{
+    private readonly TextChecker textChecker;
+
+    public Utf16Parser(TextChecker textChecker)
+    {
+        this.textChecker = textChecker;
+    }
+
+    public bool TryParse(InMemoryDeserializer deserializer, out UtfEncoding encoding)
+    {
+        if (TryParseInternal(deserializer, Endianess.BigEndian))
+        {
+            encoding = UtfEncoding.UTF16BeNoBom;
+            return true;
+        }
+        
+        if (TryParseInternal(deserializer, Endianess.LittleEndian))
+        {
+            encoding = UtfEncoding.UTF16LeNoBom;
+            return true;
+        }
+
+        encoding = UtfEncoding.Unknown;
+        return false;
+    }
+    
+    private bool TryParseInternal(InMemoryDeserializer deserializer, Endianess endianess)
+    {
+        deserializer.Offset = 0;
+        deserializer.SetEndianess(endianess);
+        
+        while (deserializer.CanRead(sizeof(ushort)))
+        {
+            if (!textChecker.IsValidCodepoint(GetNextCodepoint(deserializer)))
+                return false;
+        }
+
+        return true;
+    }
+
+    private static uint GetNextCodepoint(InMemoryDeserializer deserializer)
+    {
+        var current = deserializer.ReadUShort();
+        
+        if (current >= 0xd800 && current < 0xdc00)
+        {
+            if (!deserializer.TryReadUShort(out var next))
+                throw new DeserializerException("Unexpected end of utf16 string.");
+            return ((current & (uint)0x3ff) << 10) + (next & (uint)0x3ff) + (uint)0x10000;
+        }
+
+        return current;
+    }
+}
