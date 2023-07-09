d1bf49ba671783de9748b55f2f6b4ae37d039584
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-09 19:02:09 +0500

    CLI app entry point

--- FormatParser.CLI/Program.cs
+++ FormatParser.CLI/Program.cs
@@ -1,8 +1,45 @@
-﻿public class EntryPoint
-{
+﻿using FormatParser;
+using FormatParser.CLI;
+using FormatParser.ELF;
+using FormatParser.MachO;
+using FormatParser.PE;
+using FormatParser.Text;
 
-    public static int Main(string[] args)
+public class EntryPoint
+{
+    public static void Main(string[] args)
     {
-        return 0;
+        var directory = args[0];
+
+        var textChecker = new TextChecker();
+        var codepointConverter = new CodepointConverter();
+        var textParserSettings = TextParserSettings.Default;
+        var formatParserSetting = new FormatParserCliSettings
+        {
+            Directory = directory
+        };
+        
+        var runner = new CLIRunner(
+            new FileDiscoverer(), 
+            new FormatDecoder(new IBinaryFormatDecoder[]{new ElfDecoder(), new MachODecoder(), new PEDecoder()}, 
+                new CompositeTextFormatDecoder(new IUtfDecoder[]
+                {
+                    new Utf8Decoder(textChecker, codepointConverter, textParserSettings),
+                    new Utf16Decoder(textChecker, codepointConverter, textParserSettings),
+                }, new ITextBasedFormatDetector[]
+                {
+                    new XmlDecoder()
+                }, textParserSettings),new FormatDecoderSettings(textParserSettings, 8192)
+                )
+            );
+
+        var cts = new CancellationTokenSource();
+        var state = new ForamatParserCliState();
+        runner.Run(formatParserSetting, state, cts.Token).GetAwaiter().GetResult();
+        
+        foreach (var (formatInfo, occurence) in state.Occurence)
+        {
+            Console.WriteLine($"[{occurence.Read().ToString(),6}] : {formatInfo}");
+        }
     }
 }
