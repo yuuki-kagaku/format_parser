2e89958903c2f4b9125c6ada4d99bbbd23502a48
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-09 23:43:13 +0500

    format info for dos mz files

---
+++ FormatParser.PE/DosMzFileFormatInfo.cs
@@ -0,0 +1,11 @@
+namespace FormatParser.PE;
+
+public record DosMzFileFormatInfo() : IFileFormatInfo
+{
+    public bool Equals(IFileFormatInfo? other) => other is DosMzFileFormatInfo dosMzFileFormatInfo && this.Equals(dosMzFileFormatInfo);
+    
+    public string ToPrettyString()
+    {
+        return $"DOS MZ executable";
+    }
+}
