4867edc83e6c74c59af2d5a70cab3a8bd0c37012
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-15 20:50:28 +0500

    added more DetectionProbabilities

--- FormatParser/Text/DetectionProbability.cs
+++ FormatParser/Text/DetectionProbability.cs
@@ -3,7 +3,10 @@ namespace FormatParser.Text;
 public enum DetectionProbability
 {
     No = 0,
+    Lowest = 1,
     Low = 20,
+    MediumLow = 30,
     Medium = 40,
+    MediumHigh = 50,
     High = 60,
 }
