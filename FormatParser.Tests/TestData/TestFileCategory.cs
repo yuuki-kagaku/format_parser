50d69eaea8903d693be4b9df1539319cea864538
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-16 20:17:49 +0500

    tests refactoring

---
+++ FormatParser.Tests/TestData/TestFileCategory.cs
@@ -0,0 +1,22 @@
+namespace FormatParser.Tests.TestData;
+
+public enum TestFileCategory
+{
+    LinuxAmd64,
+    LinuxArm64,
+    LinuxArmEl,
+    LinuxArmHf,
+    LinuxI386,
+    LinuxMips,
+    LinuxMips64El,
+    LinuxMipsEl,
+    LinuxPPC64El,
+    LinuxS390X,
+    Mac,
+    MacFat,
+    PeManaged,
+    PeWindows,
+    Text,
+    PseudoText,
+    TextUtf16,
+}
