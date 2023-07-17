7874303e7c15bb743d67708f015f105af576538f
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-17 21:21:08 +0500

    refactoring in projects structure

---
+++ FormatParser.Test.Helpers/ShellRunner.cs
@@ -0,0 +1,24 @@
+namespace FormatParser.Test.Helpers;
+
+public static class ShellRunner
+{
+    public static string RunCommand(string command) => RunCommand(command, String.Empty);
+    public static string RunCommand(string command, string args)
+    {
+        var process = new System.Diagnostics.Process();
+        var startInfo = new System.Diagnostics.ProcessStartInfo
+        {
+            WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+            FileName = command,
+            Arguments = args ,
+            RedirectStandardOutput = true,
+        };
+        process.StartInfo = startInfo;
+        process.Start();
+        
+        var output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        return output;
+    }
+}
