f8dce147dfea38702c93b969d77f1924f8813a40
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-19 13:56:18 +0500

    added documentation for counterintuitive heuristic

--- FormatParser.Utf/TextAnalyzers/UTF16Heuristics.cs
+++ FormatParser.Utf/TextAnalyzers/UTF16Heuristics.cs
@@ -3,6 +3,16 @@ using FormatParser.Text.Helpers;
 
 namespace FormatParser.Text.TextAnalyzers;
 
+// Heuristics based on next assumptions:
+// There is decent probability, that file with random bytes could be interpreted as valid UTF-16
+// There are 65536 codepoints in Basic Multilingual Plane, most of this codepoint used by CJK characters: 27584 CJK Unified Ideographs, 11184 Hangul syllables
+// But in real text more than 99.85% of the text consists of 1093 mostly used syllables for Korean language
+// In modern Japanese and Chinese it estimated, that regular text usually contains only around 5000 most used CJK Unified Ideographs.
+// Rarely used CJK Unified Ideographs and Hangul syllables according to our estimation contains around 30000 characters.
+// If there are a lot of this characters in file, we can safely assume it is probably not a UTF-16 file
+// At current iteration heuristics works with non-CJK text and with modern text in CJK languages (including texts with mixed languages).
+// It is possible, that for some Chinese and Japanese text with high usage of irregular characters this heuristics required some tuning.
+
 public class UTF16Heuristics : ITextAnalyzer
 {
     private readonly HashSet<char> commonCjkBmpCharacters;
@@ -48,15 +58,18 @@ public class UTF16Heuristics : ITextAnalyzer
         var unusualCjkBmpCharsFrequency = (double)unusualCjkBmpChars / (double)totalChars;
         var commonAsciiCharsFrequency = (double)commonAsciiChars / (double)totalChars;
 
-        if (nonBmpCharsFrequency + commonAsciiCharsFrequency > 0.50)
+        if (nonBmpCharsFrequency + commonAsciiCharsFrequency > NonBmpAndCommonCharactersFrequencyThreshold)
             return DetectionProbability.Low;
 
-        if (unusualCjkBmpCharsFrequency > 0.10)
+        if (unusualCjkBmpCharsFrequency > unusualCjkBmpCharsFrequencyThreshold)
             return DetectionProbability.No;
         
         return DetectionProbability.Low;
     }
 
+    private double NonBmpAndCommonCharactersFrequencyThreshold => 0.50;
+    private double unusualCjkBmpCharsFrequencyThreshold => 0.10;
+    
     private bool IsUnusualCJKCharacter(char c)
     {
         if (commonCjkBmpCharacters.Contains(c))
