2d4300747bc1c68aa6a4694fa4ccf659428d3d26
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-07-28 15:18:38 +0500

    get rid off redundant class

--- FormatParser.MachO/MachOMagicNumbers.cs
+++ FormatParser.MachO/MachOMagicNumbers.cs
@@ -7,17 +7,17 @@ namespace FormatParser.MachO;
 internal static class MachOMagicNumbers
 {
     public static readonly IReadOnlyDictionary<byte[], (Bitness bitness, Endianness Endianess, bool IsFat)> NonFat =
-        new Dictionary<byte[], (Bitness bitness, Endianness Endianess, bool IsFat)>(ArrayComparer<byte>.Instance)
+        new Dictionary<byte[], (Bitness bitness, Endianness Endianess, bool IsFat)>(SequentialCollectionComparer<byte>.Instance)
         {
             { new byte[] { 0xFE, 0xED, 0xFA, 0xCE }, (Bitness.Bitness32, Endianness.BigEndian, false) },
             { new byte[] { 0xCE, 0xFA, 0xED, 0xFE }, (Bitness.Bitness32, Endianness.LittleEndian, false) },
             { new byte[] { 0xFE, 0xED, 0xFA, 0xCF }, (Bitness.Bitness64, Endianness.BigEndian, false) },
             { new byte[] { 0xCF, 0xFA, 0xED, 0xFE }, (Bitness.Bitness64, Endianness.LittleEndian, false) },
-        }.ToImmutableDictionary(ArrayComparer<byte>.Instance);
+        }.ToImmutableDictionary(SequentialCollectionComparer<byte>.Instance);
 
     public static readonly IReadOnlyDictionary<byte[], (Bitness bitness, Endianness Endianess, bool IsFat)>
         All = NonFat
-            .ToImmutableDictionary(ArrayComparer<byte>.Instance)
+            .ToImmutableDictionary(SequentialCollectionComparer<byte>.Instance)
             .Add(new byte[] { 0xCA, 0xFE, 0xBA, 0xBE }, (Bitness.Bitness32, Endianness.BigEndian, true))
             .Add(new byte[] { 0xBE, 0xBA, 0xFE, 0xCA }, (Bitness.Bitness32, Endianness.LittleEndian, true))
             .Add(new byte[] { 0xCA, 0xFE, 0xBA, 0xBF }, (Bitness.Bitness64, Endianness.BigEndian, true))
