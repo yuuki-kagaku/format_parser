9bfeb2c5977d0a4094bf59af60ca7f5d5ce23da8
yuuki_kagaku <yuuki_kagaku@outlook.com>
2023-08-09 20:47:00 +0500

    minor prettifications

--- FormatParser.Core/Text/EncodingHelper.cs
+++ FormatParser.Core/Text/EncodingHelper.cs
@@ -6,20 +6,25 @@ namespace FormatParser.Text;
 public static class EncodingHelper
 {
     private static readonly IReadOnlySet<string> EncodingsWithBom;
+    public static readonly IReadOnlyDictionary<string, string> CanonicalEncodingNames;
     
     static EncodingHelper()
     {
         var textDecoders = ClassDiscoveryHelper.GetAllInstancesOf<ITextDecoder, TextFileParsingSettings>(new TextFileParsingSettings()).ToArray();
 
         var encodingsWithBom = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+        var knownEncodings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
 
         foreach (var textDecoder in textDecoders)
         {
             if (textDecoder.SupportBom)
                 encodingsWithBom.Add(textDecoder.EncodingName);
+            
+            knownEncodings.Add(textDecoder.EncodingName, textDecoder.EncodingName);
         }
 
         EncodingsWithBom = encodingsWithBom;
+        CanonicalEncodingNames = knownEncodings;
     }
     
     public static bool SupportBom(string name) => EncodingsWithBom.Contains(name);
--- FormatParser.Core/Text/Helpers/EncodingInfoExtensions.cs
+++ FormatParser.Core/Text/Helpers/EncodingInfoExtensions.cs
@@ -5,8 +5,13 @@ namespace FormatParser.Text.Helpers;
 public static class EncodingInfoExtensions
 {
     public static string ToPrettyString(this EncodingInfo encodingInfo, bool alwaysPrintBom) => 
-        $"{encodingInfo.Name}{EndiannessInfo(encodingInfo)}{BomInfo(encodingInfo, alwaysPrintBom)}";
+        $"{Name(encodingInfo.Name)}{EndiannessInfo(encodingInfo)}{BomInfo(encodingInfo, alwaysPrintBom)}";
 
+    private static string Name(string name)
+    {
+        return EncodingHelper.CanonicalEncodingNames.TryGetValue(name, out var normalizedName) ? normalizedName : name;
+    }
+    
     private static string BomInfo(EncodingInfo encodingInfo, bool alwaysPrintBom)
     {
         if (!alwaysPrintBom && !EncodingHelper.SupportBom(encodingInfo.Name))
